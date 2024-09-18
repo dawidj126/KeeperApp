90fee7be5c8e2aae96b6cfebdc51f1dbb272baad
dawidj126 <dawidj126@gmail.com>
2024-09-18 22:44:06 +0200

    modified styling

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import { Zoom } from "@mui/material";
+import Add from "@mui/icons-material/Add";
 
 function CreateArea(props) {
+	const [isExpanded, setExpanded] = useState(false);
+
 	const [note, setNote] = useState({
 		title: "",
 		content: "",
@@ -27,23 +33,34 @@ function CreateArea(props) {
 		event.preventDefault();
 	}
 
+	function expand() {
+		setExpanded(true);
+	}
+
 	return (
 		<div>
-			<form>
-				<input
-					name='title'
-					onChange={handleChange}
-					value={note.title}
-					placeholder='Title'
-				/>
+			<form className='create-note'>
+				{isExpanded && (
+					<input
+						name='title'
+						onChange={handleChange}
+						value={note.title}
+						placeholder='Title'
+					/>
+				)}
 				<textarea
 					name='content'
+					onClick={expand}
 					onChange={handleChange}
 					value={note.content}
 					placeholder='Take a note...'
-					rows='3'
+					rows={isExpanded ? 3 : 1}
 				/>
-				<button onClick={submitNote}>Add</button>
+				<Zoom in={true}>
+					<Fab onClick={submitNote}>
+						<AddIcon />
+					</Fab>
+				</Zoom>
 			</form>
 		</div>
 	);
